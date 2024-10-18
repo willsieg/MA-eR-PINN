cd4b341064633a9b5ee0675193fd4bc7cb6c190b
SIEGLEW <william.siegle@daimlertruck.com>
2024-10-18 13:21:56 +0200

    update

---
+++ data/__init__.py
@@ -0,0 +1,17 @@
+import platform
+from pathlib import Path
+
+# SET SOURCE PATHS HERE:
+def get_data_path():
+    """
+    Returns the data path based on the operating system.
+    """
+    if platform.system().lower() == 'windows':
+        data_path = Path('C:/', 'Users', 'SIEGLEW', 'OneDrive - Daimler Truck', 'MA', 'Code', 'DATA')
+    elif platform.system().lower() == 'linux':
+        data_path = Path('/home', 'sieglew', 'data')
+    else:
+        print("Running on an unsupported platform")
+        data_path = None
+        
+    return data_path
