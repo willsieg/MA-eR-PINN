7d411d99f47e9f6b4a410a9f38e6370823a5a4cc
willsieg <williamsiegle@gmail.com>
2024-10-26 21:51:43 +0200

    update preprocess

--- data/preprocess_trips.py
+++ data/preprocess_trips.py
@@ -24,12 +24,12 @@ from src.physics_model.VehModel import CreateVehicle
 # Specify Data Locations:
 # FILE SOURCES ---------------------------------------------------------------
 parquet_folder = Path(DATA_PATH, "processed") # Trip parquet files
-parquet_folder_resampled = Path(DATA_PATH, "processed_resampled") # Trip parquet filesn for resampled time series
 save_model_folder = Path(ROOT, "src", "models", "pth")
 
 # ------------------------------------------------------------------------------------------------------------------------------
 # OUTPUT LOCATIONS ---------------------------------------------------------------
 new_parquet_folder = Path(DATA_PATH, "processed_new") # Trip parquet files
+parquet_folder_resampled = Path(DATA_PATH, "processed_resampled") # Trip parquet filesn for resampled time series
 pickle_destination_folder = Path(DATA_PATH, "TripFiles") # Trip parquet files
 
 '''
@@ -53,6 +53,7 @@ for f in all_files:
 
     vehicle_id = f[8:10].strip("_t")
     file_code = f[7:-8]
+    prefix = f[:7]
 
     df = pd.read_parquet(Path(parquet_folder, f), engine='fastparquet')
     df.sort_index(axis=1, inplace=True)
@@ -91,14 +92,9 @@ for f in all_files:
         if len({x for x in df[col] if x==x}) == 1:      # number of distinct non-NaN-values
             constants[col] = df[col].iloc[df[col].first_valid_index()] 
 
-    # TIME:
-    ###############################################
     # correct timestamp if necessary:
     if max(df.signal_time).year < 2000:
         df.signal_time = pd.to_datetime(df.signal_ts * (10**3))
-
-    time = pd.DataFrame(np.array((df.signal_time - df.signal_time[0]).dt.seconds))    # TIME since trip Start [s] --> corresponding to time series indices
-    time_unix = df.signal_time  # UNIX TIME (starting 1970) [s]
 
     # RESAMPLING: --> separate directory
     ###############################################
@@ -107,56 +103,50 @@ for f in all_files:
     df_resampled.reset_index(inplace=True)  # Reset the index if you want 'signal_time' to be a column again
     #df_resampled.fillna(df.median(), inplace=True)  # Basic Median Filling
 
+    # TIME:
+    ###############################################
+    time = pd.DataFrame(np.array((df.signal_time - df.signal_time[0]).dt.seconds))    # TIME since trip Start [s] --> corresponding to time series indices
+    time_unix = df.signal_time  # UNIX TIME (starting 1970) [s]
     # VEHICLE MOTION:
     ###############################################
     dist = ((df.hirestotalvehdist_cval_icuc - df.hirestotalvehdist_cval_icuc.iloc[0]) * 1000).round(3)   # MILEAGE since trip Start [m]
     speed = df.vehspd_cval_cpc/3.6                                  # VEHICLE SPEED [m/s]
     accel = pd.DataFrame(np.diff(speed, prepend = speed.iloc[0]))        # LONGITUDINAL VEHICLE ACCELERATION [m/s^2] 
-
     # ROUTE: 
     ###############################################
     gps_pos = pd.DataFrame(tuple(zip(df['latitude_cval_ippc'], df['longitude_cval_ippc'])))  
     alt = df.altitude_cval_ippc                                           # ALTITUDE [m]  
     road_grad = pd.DataFrame(df.bs_roadincln_cval)                        # ROAD GRADIENT [%] = [tan(alpha) * 100]   
     amb_temp = pd.DataFrame(savgol_filter(df.ambtemp_cval_pt, 100, 3))    # ambient temperature [°C] [smoothed]
-
     # BATTERY:
     ###############################################
     soc = df.hv_bat_soc_cval_bms1/100                  # Battery State of Charge [-]
-
     if "hv_bat_soh_cval_bms1" in constants.keys():
         soh = constants["hv_bat_soh_cval_bms1"]/100
     else:
         soh = np.mean(df.hv_bat_soh_cval_bms1)/100          # Battery State of Health [-] (constant) 
-
     bat_pwr = df.hv_batpwr_cval_bms1                # Battery Power [kW]  = df.hv_bat_dc_momvolt_cval_bms1  *  df.hv_batcurr_cval_bms1  = U*I
     bat_mom_en = df.hv_batmomavldischrgen_cval_1    # Momentary Available discharge energy [kWh]
     bat_cap_total = np.mean(bat_mom_en + df.hv_batmomavlchrgen_cval_bms1)  # Total battery capacity (constant) [kWh]
     # calculated by mean of sum of momentary available charge and discharge energies
-
     # MOTOR:
     ###############################################
     mot_1_speed = df.actualspeed_pti1               # Motor 1 Speed [rpm]
     mot_1_torque = df.actualtorque_pti1             # Motor 1 Torque [Nm] (or [%] ?)
-
     mot_2_speed = df.actualspeed_pti1               # Motor 2 Speed [rpm]
     mot_2_torque = df.actualtorque_pti1             # Motor 2 Torque [Nm] (or [%] ?)
-
     mot_pwr = df.emot_pwr_cval                      # Electrical power of motors (combined) [kW]
-
     # VEHICLE:
     ###############################################
     V = CreateVehicle(vehicle_id)        # import vehicle parameters
-
     vehweight = df.vehweight_cval_pt*1000  # weight (t) (PT) [kg]
     grocmb = df.grocmbvehweight_cval       # gross combination weight (t) [kg]
-
     try:
         weight_est_mean = sum(np.multiply(grocmb,dist))/sum(dist)       # approximated vehicle weight [kg]
     # if 'grocombvehweight' is empty:
     except:
         weight_est_mean = np.mean(vehweight)
-
+        
     # COLLECTED TIME SERIES DATA 
     ############################################################################################
     T = pd.concat([time, time_unix, dist, speed, accel, gps_pos, alt, road_grad, amb_temp,soc,bat_pwr,bat_mom_en, 
@@ -202,3 +192,5 @@ for f in all_files:
     ############################################################################################
     #df.to_parquet(f'{new_parquet_folder}/{file_code}.parquet')
     #df_resampled.to_parquet(f'{parquet_folder_resampled}/{file_code}.parquet')
+
+
